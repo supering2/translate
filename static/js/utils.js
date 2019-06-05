2ef6769c03d5a23d8cd28b74ef88786c8369f814
JudySum <3430535904@qq.com>
2019-06-06 00:59:14 +0800

    comment

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,12 +1,6 @@
-// 工具函数, 函数使用const是比较好的选择， 因为一般函数你都不想被别人覆盖或者修改
-
 const e = selector => document.querySelector(selector);
 
 const bind = (selector, eventName, callback) => {
   let element = e(selector);
   element.addEventListener(eventName, callback);
 };
-
-// const let var
-// let a
-// let a
